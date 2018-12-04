c54716c64588352f9996a8482211eac838f4cb54
Dave Avis <charles.avis@springbranchisd.com>
2018-12-04 11:26:09 -0600

    Fixed where the brackets in main delcaration are.

--- ArraySmallest.java
+++ ArraySmallest.java
@@ -19,7 +19,7 @@ public class ArraySmallest
         return smallest;
     }
 
-    public static void main( String args[] )
+    public static void main( String[] args )
     {
         System.out.println( getSmallest( new int[]{-99,1,2,3,4,5,6,7,8,9,10,12345} ) );
         System.out.println( getSmallest( new int[]{10,9,8,7,6,5,4,3,2,1,-99} ) );
